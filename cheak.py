3cedefaff9750b7b44a740dda847023fdc77ad3c
kyuchai <65773553+kyuchai@users.noreply.github.com>
2025-11-06 12:37:17 +0800

    Delete cheak.py

--- cheak.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("🔥 PyTorch 版本：", torch.__version__)
-print("💻 CUDA 是否可用：", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("🚀 GPU 名稱：", torch.cuda.get_device_name(0))
