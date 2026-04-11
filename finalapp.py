c7a4c0267706727092ee3e315e4bb4886224f516
kyuchai <65773553+kyuchai@users.noreply.github.com>
2026-04-11 08:34:48 +0800

    Add files via upload

--- finalapp.py
+++ finalapp.py
@@ -538,3 +538,4 @@ app = gr.mount_gradio_app(fastapi_app, demo, path="/")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "7860"))
     demo.launch(server_name="0.0.0.0", server_port=port)
+
